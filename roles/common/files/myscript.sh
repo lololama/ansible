5f5cc22c46bebd579d5d5c04360590a163e19ef4
JVostry <hvostry@gmail.com>
2025-01-15 10:32:25 +0100

    testing common tasks script

--- roles/common/files/myscript.sh
+++ roles/common/files/myscript.sh
@@ -1,2 +1,15 @@
-touch /tmp/iwashere.txt
-echo "I was Here" > /tmp/iwashere.txt
+#!/bin/bash
+
+LOGFILE="/var/log/myscript.log"
+
+# Ensure the log file exists and is writable
+touch "$LOGFILE"
+chmod 644 "$LOGFILE"
+
+echo "Starting myscript.sh at $(date)" >> "$LOGFILE"
+
+# Infinite loop to log timestamp every 10 seconds
+while true; do
+  echo "Current time: $(date)" >> "$LOGFILE"
+  sleep 10
+done
